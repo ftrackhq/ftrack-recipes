8c6a3aad334e846fffa4e6e88e853824fafed7f7
Steve Petterborg <steve.petterborg@ftrack.com>
2019-04-03 18:58:42 -0700

    Change behavior-switching logic, update comments

--- python/actions/annotate_review_clips/hook/rename_clips_action.py
+++ python/actions/annotate_review_clips/hook/rename_clips_action.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # :coding: utf-8
 # :copyright: Copyright (c) 2019 ftrack
 
@@ -8,7 +8,16 @@ from ftrack_action_handler.action import BaseAction
 import ftrack_api
 
 
+RUN_AS_ACTION = True  # Set to False to run as an event listener.
+UPDATE_LABELS = True  # Set to False to create comments instead.
+
+
 class UpdateReviewClips(BaseAction):
+    '''Demonstrates two methods of marking review clips with the name of the
+    original media they represent. While presented as an action, the
+    proces_event() method is suitable for use as an event listener, running on
+    clips as they are added to a review.
+    '''
     label = 'Update Clips'
     identifier = 'com.ftrack.recipes.update_clips'
     description = 'Update review session clips based on original filename'
@@ -42,6 +51,14 @@ class UpdateReviewClips(BaseAction):
         return True
 
     def _update_review_session_object(self, review_session_object):
+        '''Update a *review_session_object* with the uploaded media's filename.
+
+        We find the filename on the metadata associated with the web-reviewable
+        Component on the version which corresponds to *review_session_object*.
+
+        *review_session_object* is a ReviewSessionObject representing the clip
+        in question.
+        '''
         review_component = self.session.query(
             'select metadata from Component where name is ftrackreview-mp4'
             ' and version_id is "{0}"'.format(
@@ -52,23 +69,51 @@ class UpdateReviewClips(BaseAction):
             'Component',
             review_component['metadata']['source_component_id']
         )
-        ## Chose one
-        self._update_clip_label(review_session_object, source_component['name'])
-        # self.comment_on_clip(review_session_object, source_component['name'])
+
+        if UPDATE_LABELS:
+            self._update_clip_label(
+                review_session_object, source_component['name']
+            )
+        else:
+            self.comment_on_clip(
+                review_session_object, source_component['name']
+            )
 
     def _comment_on_clip(self, review_session_object, source_name):
+        '''Create a comment with content *source_name* on a clip.
+
+        *review_session_object* is a ReviewSessionObject representing the clip
+        in question.
+
+        *source_name* is a string representing the desired label or filename.
+        '''
         current_user = self.session.query(
             u'User where username is {0}'.format(self.session.api_user)).one()
         review_session_object.create_note(source_name, current_user)
 
     def _update_clip_label(self, review_session_object, source_name):
-        # These three fields control the label on the clips in the timeline.
+        '''Update fields comprising the label for a ReviewSessionObject clip.
+
+        The three attributes which are displayed over clips in a ReviewSession
+        timeline are "name", "description", and "version". Technically all
+        optional, depending on how the clip was created and what its parent is,
+        one or two may start out blank.
+
+        *review_session_object* is a ReviewSessionObject representing the clip
+        in question.
+
+        *source_name* is a string representing the desired label or filename.
+        '''
         review_session_object['name'] = ''
         review_session_object['description'] = ''
         review_session_object['version'] = source_name
 
     def process_event(self, event):
-        '''When a clip is added to a review, update it with the filename.'''
+        '''When a clip is added to a review, update it with the filename.
+
+        *event* is an ftrack_api.event.base.Event object.
+
+        '''
         for entity in event['data']['entities']:
             if entity['action'] != 'add':
                 continue
@@ -94,17 +139,17 @@ def register(api_object, **kw):
         return
 
     action = UpdateReviewClips(api_object)
-    ## Chose one
-    # To enable an action witin a client review
-    action.register()
-    # To automatically update clips
-    # api_object.event_hub.subscribe('topic=ftrack.update', action.process_event)
+
+    if RUN_AS_ACTION:
+        action.register()
+    else:
+        api_object.event_hub.subscribe(
+            'topic=ftrack.update', action.process_event
+        )
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    # Remember, in version version 2.0 of the ftrack-python-api the default
-    # behavior will change from True to False.
     session = ftrack_api.Session(auto_connect_event_hub=True)
     register(session)
     logging.info(
