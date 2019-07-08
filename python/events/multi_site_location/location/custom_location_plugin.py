7c82794e38295ebb57d4277caaae9e96328986d6
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2019-07-08 12:14:49 +0200

    refactor to register both locations with different priorities

--- python/events/multi_site_location/location/custom_location_plugin.py
+++ python/events/multi_site_location/location/custom_location_plugin.py
@@ -15,43 +15,55 @@ logger = logging.getLogger(
 current_location = os.environ.get('FTRACK_LOCATION')
 
 
-def configure_location(session, location_name, disk_prefix, event):
+def configure_location(session, location_setup, event):
     '''Listen.'''
-    location = session.ensure('Location', {'name': location_name})
 
-    location.accessor = ftrack_api.accessor.disk.DiskAccessor(
-        prefix=disk_prefix
-    )
-    location.structure = ftrack_api.structure.standard.StandardStructure()
-    location.priority = 1
+    for location_name, disk_prefix in location_setup.items():
 
-    logger.warning(
-        u'Registered location {0} at {1}.'.format(location_name, disk_prefix)
-    )
+        if not disk_prefix:
+            logger.error(
+                'No disk prefix configured for location {0}'.format(
+                    location_name
+                )
+            )
+            return
+
+        if not os.path.exists(disk_prefix) or not os.path.isdir(disk_prefix):
+            logger.error(
+                'Disk prefix for location {} does not exist.'.format(
+                    location_name
+                )
+            )
+            return
+
+        location = session.ensure('Location', {'name': location_name})
+
+        location.accessor = ftrack_api.accessor.disk.DiskAccessor(
+            prefix=disk_prefix
+        )
+        location.structure = ftrack_api.structure.standard.StandardStructure()
+        if location_name == current_location:
+            location.priority = 1  # lower value == higher priority !
+        else:
+            location.priority = 10
+
+        logger.warning(
+            u'Registered location {0} at {1} with priority {2}'.format(
+                location_name, disk_prefix, location.priority)
+        )
 
 
 def register(api_object, location_setup=None):
     '''Register location with *session*.'''
 
-    disk_prefix = location_setup.get(current_location)
-
     if not isinstance(api_object, ftrack_api.Session):
-        return
-
-    if not disk_prefix:
-        logger.error('No disk prefix configured for location.')
-        return
-
-    if not os.path.exists(disk_prefix) or not os.path.isdir(disk_prefix):
-        logger.error('Disk prefix location does not exist.')
         return
 
     api_object.event_hub.subscribe(
         'topic=ftrack.api.session.configure-location',
         functools.partial(
-            configure_location, 
-            api_object, 
-            current_location, 
-            disk_prefix
+            configure_location,
+            api_object,
+            location_setup
         )
     )
