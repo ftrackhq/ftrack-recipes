14215a1de04b9fce81ba70f67843ee8b41ff32cb
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-07-29 10:02:43 +0200

    init code from example

---
+++ python/actions/offset_tasks_time_selection/hook/offset_tasks_time.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# :coding: utf-8
+# :copyright: Copyright (c) 2021 ftrack
+
+import datetime
+import logging
+
+import ftrack_api
+from ftrack_action_handler.action import BaseAction
+
+
+class OffsetTasksTimeSelection(BaseAction):
+    label = 'offset tasks time selection'
+    identifier = 'ftrack.recipes.offset-tasks-time-selection'
+    description = 'offset tasks time selection'
+
+    def launch(self, session, entities, event):
+        
+        offset_days = int(event['data']['values']['offset'])
+        
+        for entity_type, entity_id in entities:
+            task = session.get(entity_type, entity_id)
+            task['start_date'] = task['start_date'].shift(days=offset_days)
+            task['end_date'] = task['end_date'].shift(days=offset_days)   
+        try:
+            session.commit()
+        except Exception as error:
+            return  {'success': False, 'message': str(error)}
+
+        return  {
+            'success': True, 
+            'message': 'Selected Tasks have been offset by {} days.'.format(
+                offset_days
+            )
+        }
+
+    def discover(self, session, entities, event):
+        if not entities:
+            return False
+
+        for entity_type, entity_id in entities:
+            if entity_type == 'Task':
+                return True
+
+        return False
+
+    def interface(self, session, entities, event):
+        # If "value" is present, the ui has been raised and the value been set.
+        # hence no need to re raise the ui.
+
+        if 'values' in event['data']:
+            return None
+
+        return [{
+                'label': 'day Offset',
+                'type': 'number',
+                'name': 'offset',
+                'empty_text': 1
+        }]
+
+
+def register(session):
+    if not isinstance(session, ftrack_api.session.Session):
+        return
+    
+    action = OffsetTasksTimeSelection(session)
+    action.register()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    session = ftrack_api.Session(auto_connect_event_hub=True)
+    register(session)
+    logging.info(
+        'Registered actions and listening for event. Use Ctrl-C to abort.'
+    )
+    session.event_hub.wait()
