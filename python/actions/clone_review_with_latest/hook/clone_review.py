00ab730bc2078172a7ecb2a73b5225b4079a0010
lorenzo angeli <lorenzo.angeli@backlight.co>
2024-04-09 14:34:20 +0200

    update copyright reformat with black limit entity to 1

--- python/actions/clone_review_with_latest/hook/clone_review.py
+++ python/actions/clone_review_with_latest/hook/clone_review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # :coding: utf-8
-# :copyright: Copyright (c) 2018 ftrack
+# :copyright: Copyright (c) 2024 ftrack
 
 import logging
 import ftrack_api
@@ -9,72 +9,77 @@ from ftrack_action_handler.action import BaseAction
 
 
 class CloneReview(BaseAction):
-    '''Clone review class.'''
+    """Clone review class."""
 
-    label = 'Clone review with Latest versions'
-    identifier = 'com.ftrack.recipes.clone_review'
-    description = 'Clone review and update with latest versions available.'
-    
+    label = "Clone review with Latest versions"
+    identifier = "com.ftrack.recipes.clone_review"
+    description = "Clone review and update with latest versions available."
+
     def validate_selection(self, entities):
-        '''Return True if the selection is valid.
+        """Return True if the selection is valid.
 
         Utility method to check *entities* validity.
 
-        '''
+        """
         if not entities:
             return False
 
         if len(entities) > 1:
             # works on one at time only.
             return False
-        
+
         entity_type, entity_id = entities[0]
-        if entity_type == 'ReviewSession':
+        if entity_type == "ReviewSession":
             return True
 
         return False
-    
+
     def discover(self, session, entities, event):
-        '''Return True if the action can be discovered.
+        """Return True if the action can be discovered.
 
         Check if the current selection can discover this action.
 
-        '''
+        """
         return self.validate_selection(entities)
 
     def launch(self, session, entities, event):
-        '''Return result of running action.'''
+        """Return result of running action."""
 
-        for entity_type, entity_id in entities:
-            review_session = session.get(entity_type, entity_id)
-            new_review_objects = []
-            objects = review_session['review_session_objects']
-            for object in objects:
-                latest_version  = session.query(f'AssetVersion where asset_id is {object["asset_version"]["asset_id"]} and is_latest_version is True').one()
-                new_session_object = session.create('ReviewSessionObject', {'asset_version': latest_version})
-                new_review_objects.append(new_session_object)
-
-            review_name = datetime.datetime.now().strftime("%d/%m/%Y")
-            new_list = session.create(
-                'ReviewSession', 
-                   {
-                       'name': review_name,
-                       'project_id': review_session['project_id'],
-                       'review_session_invitees': review_session['review_session_invitees'],
-                       'review_session_objects': new_review_objects,
-                       'settings': review_session['settings']
-                       
-                   }
+        entity_type, entity_id = entities[0]
+        review_session = session.get(entity_type, entity_id)
+        new_review_objects = []
+        objects = review_session["review_session_objects"]
+        for object in objects:
+            latest_version = session.query(
+                f'AssetVersion where asset_id is {object["asset_version"]["asset_id"]} and is_latest_version is True'
+            ).one()
+            new_session_object = session.create(
+                "ReviewSessionObject", {"asset_version": latest_version}
             )
-                
-            session.commit()
-            
-        return {'success': True, 'message': f'Successfully generated review with name : {review_name}.'}
-    
-        
-        
+            new_review_objects.append(new_session_object)
+
+        review_name = datetime.datetime.now().strftime("%d/%m/%Y")
+        new_list = session.create(
+            "ReviewSession",
+            {
+                "name": review_name,
+                "project_id": review_session["project_id"],
+                "review_session_invitees": review_session["review_session_invitees"],
+                "review_session_objects": new_review_objects,
+                "settings": review_session["settings"],
+            },
+        )
+
+        session.commit()
+
+        return {
+            "success": True,
+            "message": f"Successfully generated review with name : {review_name}.",
+        }
+
+
 def register(api_object, **kw):
-    '''Register hook with provided *api_object*.'''
+    """Register hook with provided *api_object*."""
 
     # Validate that session is an instance of ftrack_api.Session. If not,
     # assume that register is being called from an old or incompatible API and
@@ -86,12 +91,12 @@ def register(api_object, **kw):
     action.register()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # To be run as standalone code.
     logging.basicConfig(level=logging.INFO)
     session = ftrack_api.Session(auto_connect_event_hub=True)
     register(session)
 
     # Wait for events
-    logging.info('Registered actions and listening for events. Use Ctrl-C to abort.')
+    logging.info("Registered actions and listening for events. Use Ctrl-C to abort.")
     session.event_hub.wait()
