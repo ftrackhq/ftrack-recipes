2f075b86114d72128821c26291930df408a6c8b9
seebergs <mattias.seebergs@ftrack.com>
2024-11-04 12:28:12 +0100

    Added __main__ line

--- python/tools/create_inbox_message/create_inbox_message.py
+++ python/tools/create_inbox_message/create_inbox_message.py
@@ -9,39 +9,44 @@ logger = logging.getLogger()
 
 session = ftrack_api.Session()
 
-# List of recipients. Picks a random user for demo purposes.
-recipients_user_names = [
-    session.query(
-        f'User where username is_not {session.api_user}'
-    ).first().get('username')
-]
-logger.info(f'Recipients: {recipients_user_names}')
-message_content = 'Example message content from recipe'
+def create_inbox_message():
+    # List of recipients. Picks a random user for demo purposes.
+    recipients_user_names = [
+        session.query(
+            f'User where username is_not {session.api_user}'
+        ).first().get('username')
+    ]
+    logger.info(f'Recipients: {recipients_user_names}')
+    message_content = 'Example message content from recipe'
 
-# Query author and recipients. Note that the message will not appear if the
-# author and recipient is the same user.
-logger.info('Getting author and recipients...')
-author = session.query(f"User where username is {session.api_user}").one()
-recipients = session.query(
-    f"""User where username in ('{"','".join(recipients_user_names)}')"""
-).all()
+    # Query author and recipients. Note that the message will not appear if the
+    # author and recipient is the same user.
+    logger.info('Getting author and recipients...')
+    author = session.query(f"User where username is {session.api_user}").one()
+    recipients = session.query(
+        f"""User where username in ('{"','".join(recipients_user_names)}')"""
+    ).all()
 
-# Create note / "internal email"
-logger.info('Creating note/internal email...')
-note = session.create('Note', {
-    'author': author,
-    'content': message_content
-})
-
-# Add recipients
-logger.info('Creating recipients and adding to note/email...')
-for user in recipients:
-    recipient = session.create('Recipient', {
-        'note_id': note['id'],
-        'resource_id': user['id']
+    # Create note / "internal email"
+    logger.info('Creating note/internal email...')
+    note = session.create('Note', {
+        'author': author,
+        'content': message_content
     })
-    note['recipients'].append(recipient)
 
-# Persist changes to database / send "internal email"
-logger.info('Persisting to database/sending...')
-session.commit()
+    # Add recipients
+    logger.info('Creating recipients and adding to note/email...')
+    for user in recipients:
+        recipient = session.create('Recipient', {
+            'note_id': note['id'],
+            'resource_id': user['id']
+        })
+        note['recipients'].append(recipient)
+
+    # Persist changes to database / send "internal email"
+    logger.info('Persisting to database/sending...')
+    session.commit()
+
+
+if __name__ == '__main__':
+    create_inbox_message()
